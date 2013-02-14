403cfffb922d22c2e51445ce94a850e1ea6bdfa3
mennovanslooten <menno.vanslooten@gmail.com>
2013-02-15 00:16:53 +0100

    wrote lots of new promotional content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 (function($) {
-    //$('body').on('click', function(e) {
-        //$(this).toggleClass('grid');
-    //});
+	function initTabs() {
+		var $root = $(this);
+		var $targets = $root.children();
 
+		var $first_tab;
+		var $first_target;
+		var $tabs = $();
+
+		$targets.each(function() {
+			var $target = $(this);
+			var $tab = $target.children().first();
+			$tabs = $tabs.add($tab);
+
+			$first_tab = $first_tab || $tab;
+			$first_target = $first_target || $target;
+			
+			$tab.addClass('tabs-tab');
+			$target.addClass('tabs-target');
+
+			$tab.insertBefore($first_target);
+
+			$tab.on('click', function() {
+				$tabs.removeClass('active');
+				$tab.addClass('active');
+				$targets.hide();
+				$target.show();
+			});
+		});
+
+		$tabs.first().trigger('click');
+	}
+
+	$('.tabs').each(initTabs);
 })(jQuery, undefined);
